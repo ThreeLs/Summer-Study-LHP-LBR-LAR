3d079052c32fb98b824dc9dc93c0e59684f83ecd
Hanpu_Liang <vento_l@hotmail.com>
2017-07-04 19:11:41 +0800

    07-04-LHP

--- LiangHanpu/Python/JiaoWuChuChengJi/Main.py
+++ LiangHanpu/Python/JiaoWuChuChengJi/Main.py
@@ -1,16 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
 import urllib
+import urllib2
+import cookielib
+import re
 
+class CUMT:
+    
+    def __init__(self):
+        self.loginUrl = 'http://202.119.206.62/xtgl/login_login.html'
+        self.gradeUrl = 'http://202.119.206.62/cjcx/cjcx_cxDgXscj.html'
+        self.cookies = cookielib.CookieJar()
+        self.postdata = urllib.urlencode({
+                'yhm':'10154236',
+                'mm':'18103X'
+                })
+        self.opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(self.cookies))
+    
+    def getPage(self):
+        request = urllib2.Request(
+                url = self.loginUrl,
+                data = self.postdata)
+        result = self.opener.open(request)
+        #result = self.opener.open(self.gradeUrl)
+        
+        print result.read()
 
-#网址
-page = 1
-url = 'http://www.qiushibaike.com/hot.page/' + str(page)
-
-try:
-    request = urllib.request.Request(url)  #请求
-    response = urllib.request.urlopen(request)  #回应
-    print(response.read())
-except urllib.request.URLError, e:
-    if hasattr(e, "code"):
-        print(e.code)
-    if hasattr(e, "reason"):
-        print(e.reason)
+cumt = CUMT()
+cumt.getPage()
